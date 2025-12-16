965379ee7aa80f15f39332b4100734099d94571b
Yedra Garcia <ivysudadera@gmail.com>
2025-12-16 22:59:20 +0100

    ya vaaaaaaa

---
+++ backend/routes/auth.js
@@ -0,0 +1,64 @@
+// backend/routes/auth.js
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { User } = require('../db/sequelize'); 
+
+// REGISTRO
+router.post('/register', async (req, res) => {
+    const { username, email, contrasena, genero, anioNacimiento, estatura, peso } = req.body;
+    
+    try {
+        if (!contrasena) return res.status(400).json({ msg: "Contraseña requerida." });
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(contrasena, salt);
+
+        const newUser = await User.create({
+            username,
+            email,
+            contrasena: hashedPassword,
+            genero, 
+            anioNacimiento, 
+            estatura, 
+            peso
+        });
+
+        res.status(201).json({ msg: "Usuario registrado con éxito" });
+    } catch (err) {
+        console.error("❌ [REGISTER] Error:", err);
+        if (err.name === 'SequelizeUniqueConstraintError') {
+            return res.status(400).json({ msg: "El nombre de usuario o email ya están en uso." });
+        }
+        res.status(500).send('Error del servidor');
+    }
+});
+
+// LOGIN
+router.post('/login', async (req, res) => {
+    const { username, contrasena } = req.body; 
+    
+    try {
+        const user = await User.findOne({ where: { username } }); 
+        if (!user) return res.status(404).json("Usuario no encontrado");
+
+        const validPassword = await bcrypt.compare(contrasena, user.contrasena); 
+        if (!validPassword) return res.status(400).json("Contraseña incorrecta");
+        
+        // Generar Token
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+        
+        const userData = { 
+            id: user.id, 
+            username: user.username, 
+            email: user.email
+        }; 
+
+        res.status(200).json({ user: userData, token }); 
+    } catch (err) {
+        console.error("❌ [LOGIN] Error:", err);
+        res.status(500).send('Error del servidor');
+    }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,10 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const { connectDB } = require('./db/sequelize'); 
-const authRoute = require('./middleware/auth');
+//const authRoute = require('./middleware/auth');
 const routineRoute = require('./routes/routines');
-
+const authRoute = require('./routes/auth');
+const usersRoute = require('./routes/users');
 
 const app = express();
 
@@ -19,7 +20,7 @@ app.use(express.json());
 // Rutas
 app.use("/api/auth", authRoute);
 app.use("/api/routines", routineRoute);
-app.use('/api/users', require('./routes/users'));
+app.use('/api/users', usersRoute);
 
 const PORT = process.env.PORT || 42311;
 
